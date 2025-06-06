3211f9d5ce56bcf78762a0efc1d4855785f36829
dinesh-arumugam-kumar <dinesh.arumugam.kumar@gmail.com>
2025-06-06 19:53:02 +0530

    updated with basic step def

---
+++ src/main/java/pojo/LoginRequest.java
@@ -0,0 +1,43 @@
+package pojo;
+
+public class LoginRequest {
+	
+	private String userEmail;
+	private String userPassword;
+	private String token;
+	private String userId;
+	private String message;
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -1,33 +1,79 @@
 package stepDefinitions;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
 
 public class stepDefinition {
+	ResponseSpecification res;
+	RequestSpecification given_response;
+	Response response;
+	AddPlace ap;
 
 	@Given("Add Place payload")
 	public void add_place_payload() {
-	    RestAssured.baseURI="https://rahulshettyacademy.com";
-	    
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+		AddPlace p = new AddPlace();
+		p.setAccuracy(50);
+		p.setName("Frontline house");
+		p.setPhone_number("(+91) 875 470 2928");
+		p.setAddress("29, side layout, cohen 09");
+		p.setWebsite("http://google.com");
+		p.setLanguage("Tamil-IN");
+		List<String> types_list = new ArrayList<String>();
+		types_list.add("shoe park");
+		types_list.add("shop");
+		p.setTypes(types_list);
+		Location lc = new Location();
+		lc.setLat(-38.383494);
+		lc.setLng(33.427362);
+		p.setLocation(lc);
+
+		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
+		res = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+
+		given_response = given().spec(req).body(p);
+
 	}
+
 	@When("user calls {string} with post http request")
 	public void user_calls_with_post_http_request(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		if (string.equalsIgnoreCase("AddPlaceAPI")) {
+			response = given_response.when().post("maps/api/place/add/json").then().spec(res).extract().response();
+		}
 	}
+
 	@Then("the API call is success with status coded {int}")
 	public void the_api_call_is_success_with_status_coded(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		int statusCode = response.getStatusCode();
+		assertSame(statusCode, int1);
 	}
+
 	@Then("{string} in response body is {string}")
-	public void in_response_body_is(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void in_response_body_is(String key, String value) {
+		String res = response.asString();
+		JsonPath js = new JsonPath(res);
+		String keys = js.get(key).toString();
+		assertEquals(keys, value);
 	}
-
-
 
 }
