c4eb31aee42d7180ff1616e1eb9db54ec6bc5ff0
dinesh-arumugam-kumar <dinesh.arumugam.kumar@gmail.com>
2025-06-07 18:15:05 +0530

    Setting up common utils class that will give the common methods

---
+++ src/test/java/resources/Utils.java
@@ -0,0 +1,36 @@
+package resources; // This class is used to set up common utilities for API testing
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class Utils {
+	RequestSpecification req;
+	
+	public String getGlobalValue(String key) throws Exception { // This method retrieves values from a properties file
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("src/test/java/resources/global.properties");
+		prop.load(fis);
+		prop.getProperty(key);
+		return prop.getProperty(key);
+	}
+	
+	public RequestSpecification requestSpecification() throws Exception {
+		
+		PrintStream log = new PrintStream(new FileOutputStream("logging.txt")); // Initialize PrintStream to log requests & creating new file in run time& we have to add throws declaration
+		req = new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl")) // Get base URL from global.properties file
+				.addQueryParam("key", "qaclick123")
+				.addFilter(RequestLoggingFilter.logRequestTo(log)) // Log requests to console or file loggingfilter
+				.addFilter(ResponseLoggingFilter.logResponseTo(log)) // Log responses to console or file loggingfilter
+				.setContentType(ContentType.JSON).build();
+		return req;
+	}
+
+}
